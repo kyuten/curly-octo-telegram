17aa26dbdc3a5a0337bed02484074fe969e0aeca
Rerryn Veilari <rerryn@xsrpg.net>
2017-03-27 01:54:04 +0100

    Further Tweaks

--- webhook.php
+++ webhook.php
@@ -1,16 +1,24 @@
 <?php
     $secret = "Test1";
     try{
-$signature = $_SERVER['HTTP_X-HUB-SIGNATURE'];
-list($algo, $hash) = explode('=', $signature, 2);
-$payload = file_get_contents('php://input');
-$payloadHash = hash_hmac($algo, $payload, $secret);
-
-
+	$signature = $_SERVER['HTTP_X-HUB-SIGNATURE'];
+        $payload = file_get_contents('php://input');
+        list($algo, $hash) = explode('=', $signature, 2);
         $file = "/monitor/status.txt";
         $fh = fopen($file,"w");
-	fwrite($fh, $_SERVER['HTTP_X_HUB_SIGNATURE']."\n");
-	fwrite($fh, $payloadHash);
+        fwrite($fh, microtime());
+        fclose($fh);
+        $file = "/monitor/sig.txt";
+        $fh = fopen($file,"w");
+        fwrite($fh, $signature);
+        fclose($fh);
+        $file = "/monitor/payload.txt";
+        $fh = fopen($file,"w");
+        fwrite($fh, $payload;
+        fclose($fh);
+        $file = "/monitor/algo.txt";
+        $fh = fopen($file,"w");
+        fwrite($fh, $algo;
         fclose($fh);
     }
     catch(Exception $e){
